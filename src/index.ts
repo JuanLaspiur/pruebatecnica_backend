55f10c78d4098f4da99efb4ae5124d9103172769
JuanLaspiur <laspiurperezjuan@gmail.com>
2025-02-15 19:09:46 -0300

    Guardar

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
 import routes from './routes/index';
+import cors from 'cors'; 
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(`/api`, routes);
 
 const PORT: number = parseInt(process.env.PORT || '3000', 10);
